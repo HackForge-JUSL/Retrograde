55c7cdb9b01c87b7940e9f3735b82054d23c4b34
unary <kajimanirulislam50@gmail.com>
2024-03-16 23:23:30 +0530

    Co-authored-by: CodeGandalf252 <CodeGandalf252@users.noreply.github.com> Co-authored-by: SarkarSoumyajit15 <SarkarSoumyajit15@users.noreply.github.com>

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="flex justify-center items-center bg-blue-500 text-white h-20">
+      <h1 className="text-2xl font-bold">D-Vote</h1>
+      <h4 className="text-sm ml-2">Decentralized Voting Platform</h4>
+    </div>
+  );
+}
