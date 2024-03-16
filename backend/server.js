bbfddbc74127330dc6ec04d2ac4c651b031717bf
unary <kajimanirulislam50@gmail.com>
2024-03-16 12:26:08 +0530

    made the server; now connect with frontend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const connectDB = require('./config/config');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
